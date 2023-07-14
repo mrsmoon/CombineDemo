37342d85cf084fab577804703087eb713e7a2ab3
Sera <dev.sera20@gmail.com>
2023-07-14 10:06:49 -0700

    Initial Commit

---
+++ Outdoorsy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Outdoorsy
+//
+//  Created by Seher Aytekin on 7/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
