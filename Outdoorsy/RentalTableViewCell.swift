beb07da85a3e2392a860222a0b8254eabff14547
Sera <dev.sera20@gmail.com>
2023-07-14 17:24:39 -0700

    Add sfpro display regular.

---
+++ Outdoorsy/RentalTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RentalTableViewCell.swift
+//  Outdoorsy
+//
+//  Created by Seher Aytekin on 7/14/23.
+//
+
+import UIKit
+
+class RentalTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Outdoorsy/ViewController.swift
+++ Outdoorsy/ViewController.swift
@@ -8,10 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let tableView: UITableView = {
+        let table = UITableView()
+        table.register(RentalTableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    var textfield: UITextField  {
+        let textfield = UITextField()
+        return textfield
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(textfield)
+        view.addSubview(tableView)
+        
     }
 
 
